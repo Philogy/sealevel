3f7b86d10ac72df9496cad39e547dd0cc11c7ef4
Oana Barbu <akakob@gmail.com>
2026-07-25 00:05:31 +0100

    LiquidityBook

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
+use alloy_primitives::{Address, B256, Bytes, U256};
+use std::collections::HashMap;
+
+pub struct LiquidityBook {
+    pub last_processed_block: u64,
+    pub makers: HashMap<Address, Maker>,
+}
+
+pub struct Maker {
+    pub accepted_strategies: HashMap<B256, Bytes>,
+    pub tokens: HashMap<Address, HashMap<B256, U256>>,
+}
+
 fn main() {
     println!("sealevel backend");
 }
