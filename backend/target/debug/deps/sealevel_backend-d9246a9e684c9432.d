89c071bed9b1e57d8461260014a8b2650bb7af67
Oana Barbu <akakob@gmail.com>
2026-07-24 23:12:23 +0100

    add backend

---
+++ backend/target/debug/deps/sealevel_backend-d9246a9e684c9432.d
@@ -0,0 +1,5 @@
+/Users/oana/playground/sealevel/backend/target/debug/deps/sealevel_backend-d9246a9e684c9432.d: src/main.rs
+
+/Users/oana/playground/sealevel/backend/target/debug/deps/sealevel_backend-d9246a9e684c9432: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/sealevel_backend-f25312f902d09f9f.d
@@ -0,0 +1,9 @@
+/Users/oana/playground/sealevel/backend/target/debug/deps/sealevel_backend-f25312f902d09f9f.d: src/main.rs Cargo.toml
+
+/Users/oana/playground/sealevel/backend/target/debug/deps/libsealevel_backend-f25312f902d09f9f.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
