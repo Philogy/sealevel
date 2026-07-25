14cb3c5fd9fc9337682f3b5b7b553155d0323e34
philogy <philippe@dumo.net>
2026-07-25 17:38:54 +0100

    update deploy script

--- contracts/script/DeploySeaLevel.s.sol
+++ contracts/script/DeploySeaLevel.s.sol
@@ -2,25 +2,24 @@
 pragma solidity ^0.8.30;
 
 import {Script} from "forge-std/Script.sol";
-import {console2} from "forge-std/console2.sol";
+import {console} from "forge-std/console.sol";
 import {PlankDeployer, BuildOptions} from "plank-foundry-deployer/PlankDeployer.sol";
 import {Aqua} from "aqua/src/Aqua.sol";
 
 import {ISeaLevel} from "src/ISeaLevel.sol";
 
 contract DeploySeaLevel is Script, PlankDeployer {
-    function run() external returns (Aqua aqua, ISeaLevel seaLevel) {
+    function run() external {
         BuildOptions memory options = initBuildOptions().dependency("sea_level", "src");
         bytes memory seaLevelInitcode = plankBuildFFI("src/SeaLevel.plk", options);
 
         vm.startBroadcast(vm.envUint("PRIV_KEY"));
+        address aqua = vm.envAddress("AQUA");
 
-        aqua = new Aqua();
-        seaLevel = ISeaLevel(_deploy(bytes.concat(seaLevelInitcode, abi.encode(address(aqua))), 0));
+        address seaLevel = _deploy(bytes.concat(seaLevelInitcode, abi.encode(aqua)), 0);
 
         vm.stopBroadcast();
 
-        console2.log("Aqua:", address(aqua));
-        console2.log("SeaLevel:", address(seaLevel));
+        console.log("SeaLevel: %s", seaLevel);
     }
 }
