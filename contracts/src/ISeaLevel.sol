9ca9abcfb83fd21fd15ebd7c82ee73a6a1762e69
philogy <philippe@dumo.net>
2026-07-25 15:13:50 +0100

    add sol interface

---
+++ contracts/src/ISeaLevel.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @author philogy <https://github.com/philogy>
+interface ISeaLevel {
+    function swap(address maker, address tokenIn, address tokenout, uint16 feeRate, uint256 amountIn) external;
+
+    function setup_token(address token) external;
+}
