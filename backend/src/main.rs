725adc51c818257a3b1d92fb93e6d3d3165fb5f3
Oana Barbu <akakob@gmail.com>
2026-07-25 19:40:34 +0100

    only log sealevel logs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -435,7 +435,10 @@ async fn listen_for_heads(ws_url: Url, head_sender: watch::Sender<u64>) {
 
 #[tokio::main]
 async fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or("sealevel_backend=info,warn"),
+    )
+    .init();
 
     let config = match Config::load() {
         Ok(config) => config,
